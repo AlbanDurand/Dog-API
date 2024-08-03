fc647243efd03492201dfc568cffd89b7b9403e0
Alban DURAND <durand.alban@gmail.com>
2024-08-03 19:11:59 +0200

    chore(breed): register newly created implementations

--- app/Providers/BreedServiceProvider.php
+++ app/Providers/BreedServiceProvider.php
@@ -10,9 +10,20 @@ use App\Application\Breed\GetBreed\GetBreed;
 use App\Application\Breed\GetBreed\GetBreedInterface;
 use App\Application\Breed\GetRandomBreed\GetRandomBreed;
 use App\Application\Breed\GetRandomBreed\GetRandomBreedInterface;
+use App\Application\Breed\SynchronizeBreedSummaryList\DelayedBreedSummaryListSynchronization;
+use App\Application\Breed\SynchronizeBreedSummaryList\NextBreedSummaryListSynchronizationDelay;
+use App\Application\Breed\SynchronizeBreedSummaryList\SynchronizeBreedSummaryList;
+use App\Application\Breed\SynchronizeBreedSummaryList\SynchronizeBreedSummaryListInterface;
+use App\Application\TimeUnit\Minute;
 use App\Domain\Breed\BreedExternalProvider\BreedExternalProviderInterface;
+use App\Domain\Breed\BreedRepositoryInterface;
 use App\Infrastructure\BreedExternalProvider\BreedApiProvider;
+use App\Infrastructure\BreedRepository\BreedRepository;
+use App\Infrastructure\BreedSummaryListSynchronizationRepository\BreedSummaryListSynchronizationRepository;
+use App\Infrastructure\BreedSummaryListSynchronizationRepository\BreedSummaryListSynchronizationRepositoryInterface;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Clock\Clock;
 
 class BreedServiceProvider extends ServiceProvider
 {
@@ -32,6 +43,29 @@ class BreedServiceProvider extends ServiceProvider
         $this->app->singleton(GetRandomBreedInterface::class, GetRandomBreed::class);
 
         $this->app->singleton(FindBreedImagePathInterface::class, FindBreedImagePath::class);
+
+        $this->app->singleton(BreedRepositoryInterface::class, BreedRepository::class);
+
+        $this->app->singleton(
+            BreedSummaryListSynchronizationRepositoryInterface::class,
+            BreedSummaryListSynchronizationRepository::class
+        );
+
+        $this->app->singleton(SynchronizeBreedSummaryListInterface::class, SynchronizeBreedSummaryList::class);
+
+        $this->app->extend(
+            SynchronizeBreedSummaryList::class,
+            function (SynchronizeBreedSummaryList $synchronize, Application $app) {
+                return new DelayedBreedSummaryListSynchronization(
+                    $synchronize,
+                    new NextBreedSummaryListSynchronizationDelay(
+                        $app->make(BreedSummaryListSynchronizationRepositoryInterface::class),
+                        new Clock(),
+                        new Minute(30)
+                    )
+                );
+            }
+        );
     }
 
     /**
