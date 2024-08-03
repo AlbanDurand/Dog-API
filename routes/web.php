5069ff43a10d9672a58560b09083ca1c581cabdd
Alban DURAND <durand.alban@gmail.com>
2024-08-03 15:38:16 +0200

    feat(breed): create endpoints to run breed queries

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Breed\GetAllBreedsController;
+use App\Http\Controllers\Breed\GetBreedController;
+use App\Http\Controllers\Breed\GetRandomBreedController;
+use App\Http\Controllers\Breed\ShowBreedImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/breed');
+Route::get('/breed', [GetAllBreedsController::class, 'index']);
+Route::get('/breed/random', [GetRandomBreedController::class, 'index']);
+Route::get('/breed/{breedName}', [GetBreedController::class, 'index']);
+Route::get('/breed/{breedName}/image', [ShowBreedImageController::class, 'index']);
