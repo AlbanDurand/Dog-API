416759da88823cb863e3a65f788122a7e3241214
Alban Durand <durand.alban@gmail.com>
2024-08-04 18:34:28 +0200

    fix(park): correct relationship

--- app/Models/Park.php
+++ app/Models/Park.php
@@ -5,16 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Park extends Model
 {
     use HasFactory;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
     public $timestamps = false;
     protected $fillable = ['id', 'name'];
 
-    public function breeds(): MorphMany
+    public function breeds(): MorphToMany
     {
-        return $this->morphMany(Breed::class, 'breedable');
+        return $this->morphToMany(Breed::class, 'breedable');
     }
 }
