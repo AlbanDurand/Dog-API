7bc6de9b1ee1c8afecac29ad6239dc1c3b1b517e
Alban Durand <durand.alban@gmail.com>
2024-08-04 17:14:08 +0200

    feat(user): create model and recreate table

---
+++ database/migrations/2024_08_04_145843_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+            $table->string('email', 255);
+            $table->string('name', 32);
+            $table->string('location', 32);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
