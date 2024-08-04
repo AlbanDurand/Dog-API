015fd9bd95f5d4cc8f3284a8302b1c39a2a15e2a
Alban Durand <durand.alban@gmail.com>
2024-08-04 17:16:27 +0200

    feat(owner): add methods to get owners

--- app/Domain/Owner/OwnerRepositoryInterface.php
+++ app/Domain/Owner/OwnerRepositoryInterface.php
@@ -5,4 +5,11 @@ namespace App\Domain\Owner;
 interface OwnerRepositoryInterface
 {
     public function save(Owner $owner): void;
+
+    /**
+     * @return array<Owner>
+     */
+    public function getAll(): array;
+
+    public function findOne(OwnerId $id): ?Owner;
 }
--- app/Infrastructure/OwnerRepository/OwnerRepository.php
+++ app/Infrastructure/OwnerRepository/OwnerRepository.php
@@ -3,6 +3,7 @@
 namespace App\Infrastructure\OwnerRepository;
 
 use App\Domain\Owner\Owner;
+use App\Domain\Owner\OwnerId;
 use App\Domain\Owner\OwnerRepositoryInterface;
 
 final readonly class OwnerRepository implements OwnerRepositoryInterface
@@ -10,5 +11,15 @@ final readonly class OwnerRepository implements OwnerRepositoryInterface
     public function save(Owner $owner): void
     {
         // TODO: Implement save() method.
+    }
+
+    public function getAll(): array
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function findOne(OwnerId $id): ?Owner
+    {
+        // TODO: Implement findOne() method.
     }
 }
